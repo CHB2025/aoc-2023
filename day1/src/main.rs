4a835c40d02484401b44e7e0b31b4247b56f8a8f
Caleb Bareman <cbareman99@gmail.com>
2023-12-01 19:25:17 -0800

    Day 1 Part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let input = include_str!("input.txt").trim();
-    part_one(input);
+    println!("Part 1: {}", part_one(input));
+    println!("Part 2: {}", part_two(input));
 }
 
 fn part_one(input: &str) -> u32 {
@@ -14,4 +15,34 @@ fn calibrate(line: &str) -> u32 {
         .expect("at least one number should exist in the input");
     let last = chars.rev().find(|c| c.is_ascii_digit()).unwrap_or(first);
     first.to_digit(10).unwrap() * 10 + last.to_digit(10).unwrap()
+}
+
+fn part_two(input: &str) -> usize {
+    input.lines().fold(0, |acc, l| acc + calibrate_fixed(l))
+}
+
+fn calibrate_fixed(line: &str) -> usize {
+    let numbers = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1",
+        "2", "3", "4", "5", "6", "7", "8", "9",
+    ];
+    let (mut min_start, mut min_val) = (usize::MAX, 0);
+    let (mut max_start, mut max_val) = (0, 0);
+
+    for (i, num) in numbers.into_iter().enumerate() {
+        if let Some(ind) = line.find(num) {
+            if ind < min_start {
+                min_start = ind;
+                min_val = i % 10;
+            }
+
+            if let Some(ind) = line.rfind(num) {
+                if ind >= max_start {
+                    max_start = ind;
+                    max_val = i % 10;
+                }
+            }
+        }
+    }
+    min_val * 10 + max_val
 }
