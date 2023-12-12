e26e0a01e695933e6f15bedc3b2fddf26ebe1fec
Caleb Bareman <cbareman99@gmail.com>
2023-12-11 19:40:05 -0800

    Day 10, part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -44,8 +44,86 @@ fn part_one(input: &str) -> u32 {
 }
 
 fn part_two(input: &str) -> u32 {
-    let _ = input;
-    0
+    let (map, start) = parse_map(input);
+
+    let mut on_loop: Box<[Box<[bool]>]> = vec![vec![false; map[0].len()].into(); map.len()].into();
+    on_loop[start.1][start.0] = true;
+
+    let [mut left, finish] = map[start.1][start.0].as_ref().unwrap().connections(start);
+    on_loop[finish.1][finish.0] = true;
+
+    while left != finish {
+        on_loop[left.1][left.0] = true;
+        left = map[left.1][left.0]
+            .as_ref()
+            .unwrap()
+            .connections(left)
+            .into_iter()
+            .find(|p| !on_loop[p.1][p.0] || p == &finish) // immediately returning to start
+            .unwrap();
+    }
+
+    let mut count = 0;
+
+    for (y, row) in map.iter().enumerate() {
+        let mut state = false;
+        let mut start_corner = None;
+        for (x, pipe) in row.iter().enumerate() {
+            // Going east to west
+            if on_loop[y][x] {
+                let pipe = pipe.as_ref().unwrap();
+                match pipe {
+                    Pipe::Ns => state = !state,
+                    Pipe::Ew => (),
+                    // L
+                    Pipe::Ne => match start_corner {
+                        Some(_) => unreachable!(),
+                        None => start_corner = Some(Pipe::Ne),
+                    },
+                    Pipe::Nw => match start_corner {
+                        // L--J
+                        Some(Pipe::Ne) => start_corner = None,
+                        // F--J
+                        Some(Pipe::Se) => {
+                            state = !state;
+                            start_corner = None
+                        }
+                        _ => unreachable!(),
+                    },
+                    // 7
+                    Pipe::Sw => match start_corner {
+                        // L--7
+                        Some(Pipe::Ne) => {
+                            state = !state;
+                            start_corner = None;
+                        }
+                        // F--7
+                        Some(Pipe::Se) => start_corner = None,
+                        _ => unreachable!(),
+                    },
+                    Pipe::Se => match start_corner {
+                        Some(_) => unreachable!(),
+                        None => start_corner = Some(Pipe::Se),
+                    },
+                };
+            } else if state {
+                count += 1;
+            }
+        }
+        assert!(
+            start_corner.is_none(),
+            "Mismatched corner {:?} on line {}",
+            start_corner,
+            y
+        );
+        assert!(
+            !state,
+            "Expected to be out of the loop at the end of line {}",
+            y
+        )
+    }
+
+    count
 }
 
 fn parse_map(input: &str) -> (Map, Point) {
@@ -132,5 +210,28 @@ LJ..."
     #[test]
     fn part_one_complex() {
         assert_eq!(part_one(COMPLEX), 8);
+    }
+
+    #[test]
+    fn part_two_basic() {
+        assert_eq!(part_two(SIMPLE), 1);
+        assert_eq!(part_two(RH_SIMPLE), 1);
+    }
+
+    #[test]
+    fn part_two_med() {
+        let input = {
+            "...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+..........."
+        };
+
+        assert_eq!(part_two(input), 4);
     }
 }
