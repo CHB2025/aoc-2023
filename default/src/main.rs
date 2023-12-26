6b8ee425e6dc54f4b43ee53668d39d2f3a08a645
Caleb Bareman <cbareman99@gmail.com>
2023-12-25 16:18:27 -0800

    Day 20

--- default/src/main.rs
+++ default/src/main.rs
@@ -18,3 +18,20 @@ fn part_two(input: &str) -> Result<usize> {
     _ = input;
     Ok(0)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = { "" };
+
+    #[test]
+    fn part_one_basic() {
+        assert_eq!(part_one(INPUT).unwrap(), 0);
+    }
+
+    #[test]
+    fn part_two_basic() {
+        assert_eq!(part_two(INPUT).unwrap(), 0);
+    }
+}
